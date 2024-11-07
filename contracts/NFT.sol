4a62c23cecaa01b2c7d339d276824c0485a7d90c
khannannu4 <khannannu451@gmail.com>
2024-11-07 12:32:49 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("2","2")
+        ERC721("2","3")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
